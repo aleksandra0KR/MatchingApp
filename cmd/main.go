3da27a3b25cb16cabb0534d0058267106bad0838
aleksandra2022itmo <kriuchkova.alexa@yandex.ru>
2024-07-08 19:48:39 +0300

    test: test inserting

--- cmd/main.go
+++ cmd/main.go
@@ -2,22 +2,33 @@ package main
 
 import (
 	"MatchingApp/internal/model"
+	"MatchingApp/internal/repository"
+	"github.com/gofrs/uuid/v5"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"log"
 )
 
 func main() {
-	dsn := "host=localhost user=postgres dbname=MatchingApp password=postgres sslmode=disable"
+	dsn := "host=localhost port=5436 user=postgres dbname=postgres password=postgres sslmode=disable"
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatalf("cant connect to postgresql: %v", err)
 		return
 	}
 
-	err = db.AutoMigrate(&model.User{}, &model.PlayList{})
+	err = db.AutoMigrate(&model.User{}, &model.Playlist{})
 	if err != nil {
 		log.Fatalf("cant initialize to postgresql: %v", err)
 		return
 	}
+	repo := repository.NewRepository(db)
+	repo.UserRepository.CreateUser(&model.User{
+		Email:    "ewd",
+		Username: "wderf",
+		Match:    make([]model.User, 0),
+	})
+
+	uu, _ := uuid.FromString("3e7957b2-a10c-469c-8473-cd3644a6f7cf")
+	repo.PlaylistRepository.CreatePlaylist(&model.Playlist{UserID: uu, Energy: 1})
 }
