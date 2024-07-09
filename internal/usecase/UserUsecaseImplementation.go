9c521fce7681f0ec94ee677f05ee12f8a2237fb1
aleksandra2022itmo <kriuchkova.alexa@yandex.ru>
2024-07-09 22:14:42 +0300

    feat: user use cases without error support

---
+++ internal/usecase/UserUsecaseImplementation.go
@@ -0,0 +1,35 @@
+package usecase
+
+import (
+	"MatchingApp/internal/model"
+	"MatchingApp/internal/repository"
+	"fmt"
+	"github.com/gofrs/uuid/v5"
+)
+
+type UserUseCaseImplementation struct {
+	repository repository.UserRepository
+}
+
+func NewUserUseCaseImplementation(repository repository.UserRepository) *UserUseCaseImplementation {
+	return &UserUseCaseImplementation{repository: repository}
+}
+
+func (uc *UserUseCaseImplementation) CreateUser(user *model.User) {
+	if user.Email == "" || user.Password == "" || user.Username == "" {
+		fmt.Errorf("all fileds can't be empty")
+	}
+	uc.repository.CreateUser(user)
+}
+
+func (uc *UserUseCaseImplementation) DeleteUserByID(id uuid.UUID) {
+	uc.repository.DeleteUserByID(id)
+}
+func (uc *UserUseCaseImplementation) FindUserByID(id uuid.UUID) *model.User {
+	foundedUser := uc.repository.FindUserByID(id)
+
+	return foundedUser
+}
+func (uc *UserUseCaseImplementation) FindMatch(id uuid.UUID) *model.User {
+	return uc.repository.FindMatch(id)
+}
