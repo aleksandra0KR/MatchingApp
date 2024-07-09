038abf40ed771e605c34ff62dce93c67609f846d
aleksandra2022itmo <kriuchkova.alexa@yandex.ru>
2024-07-09 22:14:53 +0300

    feat: added password

--- internal/model/User.go
+++ internal/model/User.go
@@ -7,6 +7,7 @@ import (
 type User struct {
 	ID       uuid.UUID `json:"id,omitempty" gorm:"type:uuid;default:uuid_generate_v4();primaryKey"`
 	Username string    `json:"username,omitempty" gorm:"size:255"`
+	Password string    `json:"password,omitempty" gorm:"size:255"`
 	Email    string    `json:"email,omitempty" gorm:"type:varchar(100);unique_index"`
 	Match    []User    `json:"match,omitempty" gorm:"foreignKey:id"`
 }
