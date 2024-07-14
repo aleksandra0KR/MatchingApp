8db85aa3801901f8aeae7a674b6cd17682f8b41d
aleksandra2022itmo <kriuchkova.alexa@yandex.ru>
2024-07-14 15:29:16 +0300

    feat: added functions for validation and login

---
+++ internal/handler/middleware.go
@@ -0,0 +1,62 @@
+package handler
+
+import (
+	"MatchingApp/internal/model"
+	"fmt"
+	"github.com/gofrs/uuid/v5"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
+)
+
+func (h *Handler) RequireAuth(c *gin.Context) {
+	// Get the cookie off the request
+	tokenString, err := c.Cookie("Authorization")
+
+	if err != nil {
+		c.AbortWithStatus(http.StatusUnauthorized)
+	}
+
+	// Decode/validate it
+	token, _ := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		// Don't forget to validate the alg is what you expect:
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+
+		// hmacSampleSecret is a []byte containing your secret, e.g. []byte("my_secret_key")
+		return []byte(os.Getenv("SECRET")), nil
+	})
+
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		// Chec k the expiry date
+		if float64(time.Now().Unix()) > claims["exp"].(float64) {
+			c.AbortWithStatus(http.StatusUnauthorized)
+		}
+
+		// Find the user with token Subject
+		var user model.User
+		userID, err := uuid.FromString(claims["sub"].(string))
+		fmt.Println(userID)
+		if err != nil {
+			// Handle the error
+		}
+		user = *h.service.FindUserByID(userID)
+
+		if user.ID == uuid.Nil {
+			c.AbortWithStatus(http.StatusUnauthorized)
+		}
+		fmt.Println(user)
+
+		// Attach the request
+		c.Set("user", user)
+
+		//Continue
+		c.Next()
+	} else {
+		c.AbortWithStatus(http.StatusUnauthorized)
+	}
+}
--- internal/handler/userHandler.go
+++ internal/handler/userHandler.go
@@ -3,52 +3,143 @@ package handler
 import (
 	"MatchingApp/internal/model"
 	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/gofrs/uuid/v5"
+	"github.com/golang-jwt/jwt/v5"
+	"golang.org/x/crypto/bcrypt"
 	"log"
 	"net/http"
+	"os"
+	"regexp"
+	"time"
 )
 
-func (h *Handler) userHandler(w http.ResponseWriter, r *http.Request) {
-	log.Printf("%s request on %s", r.Method, r.RequestURI)
+func (h *Handler) userHandler(c *gin.Context) {
+	log.Printf("%s request on %s", c.Request.Method, c.Request.RequestURI)
 
-	switch r.Method {
+	switch c.Request.Method {
 	case http.MethodPost:
-		h.createUser(w, r)
+		if (regexp.MustCompile(`/MatchingApp/createUser*`)).MatchString(c.Request.URL.String()) {
+			h.createUser(c)
+		} else {
+			h.loginUser(c)
+		}
+
 	case http.MethodDelete:
 	case http.MethodPut:
 	case http.MethodGet:
-		h.registrationUser(w, r)
+		if (regexp.MustCompile(`/MatchingApp/login*`)).MatchString(c.Request.URL.String()) {
+			h.login(c)
+		} else if (regexp.MustCompile(`/MatchingApp/registrationUser*`)).MatchString(c.Request.URL.String()) {
+			h.registrationUser(c)
+		} else {
+			h.Validate(c)
+		}
 	default:
-		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+		c.AbortWithStatus(http.StatusMethodNotAllowed)
 	}
 }
 
-func (h *Handler) registrationUser(w http.ResponseWriter, r *http.Request) {
+func (h *Handler) registrationUser(c *gin.Context) {
 	log.Println("*****registrationUser running*****")
-	err := h.tpl.ExecuteTemplate(w, "registration.html", nil)
+	err := h.tpl.ExecuteTemplate(c.Writer, "registration.html", nil)
 	if err != nil {
+		c.AbortWithError(http.StatusInternalServerError, err)
 		return
 	}
 }
-
-func (h *Handler) createUser(w http.ResponseWriter, r *http.Request) {
-
+func (h *Handler) createUser(c *gin.Context) {
 	fmt.Println("*****registerAuthHandler running*****")
-	r.ParseForm()
-	username := r.FormValue("username")
-	password := r.FormValue("password")
-	email := r.FormValue("email")
+	username := c.PostForm("username")
+	password := c.PostForm("password")
+	email := c.PostForm("email")
 
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), 10)
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Failed to hash password.",
+		})
+		return
+	}
 	var user model.User
 	user.Username = username
-	user.Password = password
+	user.Password = string(hash)
 	user.Email = email
 
 	h.service.UserUseCase.CreateUser(&user)
 
 	log.Printf("createUser is completed")
-	w.WriteHeader(http.StatusCreated)
-	_, err := fmt.Fprint(w, "user is created ")
+	c.Status(http.StatusCreated)
+	_, err = c.Writer.Write([]byte("user is created "))
 	if err != nil {
+		c.AbortWithError(http.StatusInternalServerError, err)
 		return
 	}
+}
+
+func (h *Handler) login(c *gin.Context) {
+	log.Println("*****login running*****")
+	err := h.tpl.ExecuteTemplate(c.Writer, "login.html", nil)
+	if err != nil {
+		c.AbortWithError(http.StatusInternalServerError, err)
+		return
+	}
+}
+func (h *Handler) loginUser(c *gin.Context) {
+	fmt.Println("*****loginUser running*****")
+	username := c.PostForm("username")
+	password := c.PostForm("password")
+
+	user := h.service.FindUserByUsername(username)
+
+	if user.ID == uuid.Nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Invalid username or password",
+		})
+		return
+	}
+
+	// Compare sent in password with saved users password
+	err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password))
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Invalid email or password",
+		})
+		return
+	}
+
+	// Generate a JWT token
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"sub": user.ID,
+		"exp": time.Now().Add(time.Hour * 24 * 30).Unix(),
+	})
+
+	// Sign and get the complete encoded token as a string using the secret
+	tokenString, err := token.SignedString([]byte(os.Getenv("SECRET")))
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Failed to create token",
+		})
+		return
+	}
+
+	// Respond
+	c.SetSameSite(http.SameSiteLaxMode)
+	c.SetCookie("Authorization", tokenString, 3600*24*30, "", "", false, true)
+
+	c.JSON(http.StatusOK, gin.H{})
+
+}
+
+func (h *Handler) Validate(c *gin.Context) {
+	user, _ := c.Get("user")
+
+	// user.(models.User).Email    -->   to access specific data
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": user,
+	})
 }
