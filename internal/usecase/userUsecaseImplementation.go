84e35f30cbe01ff7d3762d15f3aa36c3e1c560d3
aleksandra2022itmo <kriuchkova.alexa@yandex.ru>
2024-08-10 18:00:51 +0300

    feat: added error handling for user service

--- internal/usecase/userUsecaseImplementation.go
+++ internal/usecase/userUsecaseImplementation.go
@@ -5,6 +5,7 @@ import (
 	"MatchingApp/internal/repository"
 	"fmt"
 	"github.com/gofrs/uuid/v5"
+	"log"
 )
 
 type UserUseCaseImplementation struct {
@@ -15,27 +16,47 @@ func NewUserUseCaseImplementation(repository repository.UserRepository) *UserUse
 	return &UserUseCaseImplementation{repository: repository}
 }
 
-func (uc *UserUseCaseImplementation) CreateUser(user *model.User) {
+func (uc *UserUseCaseImplementation) CreateUser(user *model.User) error {
 	if user.Email == "" || user.Password == nil || user.Username == "" {
-		fmt.Errorf("all fileds can't be empty")
+		return fmt.Errorf("fileds can't be empty")
 	}
-	uc.repository.CreateUser(user)
+
+	if foundedUser, err := uc.FindUserByUsername(user.Username); foundedUser != nil || err != nil {
+		return fmt.Errorf("user with username %s already exists, try another username", user.Username)
+	}
+
+	err := uc.repository.CreateUser(user)
+	return err
 }
 
-func (uc *UserUseCaseImplementation) DeleteUserByID(id uuid.UUID) {
-	uc.repository.DeleteUserByID(id)
+func (uc *UserUseCaseImplementation) DeleteUserByID(id uuid.UUID) error {
+	foundedUser, err := uc.FindUserByID(id)
+	if err != nil {
+		return err
+	} else if foundedUser == nil {
+		log.Printf("can't delete user with id %s, there is no such user", id)
+		return fmt.Errorf("can't delete user with id %s, there is no such user", id)
+	}
+	err = uc.repository.DeleteUserByID(id)
+	return err
 }
-func (uc *UserUseCaseImplementation) FindUserByID(id uuid.UUID) *model.User {
-	foundedUser := uc.repository.FindUserByID(id)
 
-	return foundedUser
+func (uc *UserUseCaseImplementation) FindUserByID(id uuid.UUID) (*model.User, error) {
+	foundedUser, err := uc.repository.FindUserByID(id)
+	if err != nil {
+		return nil, err
+	}
+	return foundedUser, nil
 }
 
-func (uc *UserUseCaseImplementation) FindUserByUsername(username string) *model.User {
-	foundedUser := uc.repository.FindUserByUsername(username)
-
-	return foundedUser
+func (uc *UserUseCaseImplementation) FindUserByUsername(username string) (*model.User, error) {
+	foundedUser, err := uc.repository.FindUserByUsername(username)
+	if err != nil {
+		return nil, err
+	}
+	return foundedUser, nil
 }
+
 func (uc *UserUseCaseImplementation) FindMatch(id uuid.UUID) *model.User {
 	return uc.repository.FindMatch(id)
 }
